8033eb9868426597cd65426b060fa8e3c57e9585
Mephisto5558 <87113906+Mephisto5558@users.noreply.github.com>
2025-01-25 00:52:02 +0100

    Initial commit: config files

---
+++ eslint.config.mjs
@@ -0,0 +1,24 @@
+import config from '@mephisto5558/eslint-config';
+import globals from 'globals'
+
+/**
+ * @type { import('eslint').Linter.Config[] }
+ * This config lists all rules from every plugin it uses. */
+export default [
+  ...config,
+  {
+    name: 'overwrite',
+    files: ['**/*.js', '**/*.html'],
+    languageOptions: {
+      globals: {
+        ...globals.es2024,
+        ...globals.browser,
+        Swal: 'readable',
+        clickCount: 'writeable'
+      }
+    },
+    rules: {
+      '@typescript-eslint/no-unused-vars': 'off'
+    }
+  }
+];
