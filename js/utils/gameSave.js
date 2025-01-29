2872a962023aac89727dc35716ef629c2aa5b7d5
Mephisto5558 <87113906+Mephisto5558@users.noreply.github.com>
2025-01-29 23:56:09 +0100

    added exportSave function

--- js/utils/gameSave.js
+++ js/utils/gameSave.js
@@ -29,11 +29,22 @@ export function deleteSave() {
   globalThis.location.reload();
 }
 
-export function importSave() {
-  createElement('input', { type: 'file', accept: '.json', onchange: e => {
-    try { loadGame(e.target.files[0].text()); }
+export async function importSave() {
+  await Swal.fire('User interaction required', 'Click ok to open the file selector', 'info');
+  createElement('input', { type: 'file', accept: '.save', onchange: async e => {
+    try { loadGame(atob(await e.target.files[0].text())); }
     catch (err) {
       void Swal.fire('Invalid File', `The selected file is not a valid save file.<br>${err.toString()}`, 'error');
     }
   } }).click();
+}
+
+export function exportSave() {
+  saveGame(); // update on localStorage
+
+  const url = URL.createObjectURL(new Blob([globalThis.localStorage.getItem('saveState')], { type: 'text/plain' }));
+
+  createElement('a', { href: url, download: `webgame_${new Date().toISOString()}.save` }).click();
+
+  URL.revokeObjectURL(url); // clear up space
 }
