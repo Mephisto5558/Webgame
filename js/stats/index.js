fe448ba98bb1896b50f40c657863425ce8072fcf
Mephisto5558 <87113906+Mephisto5558@users.noreply.github.com>
2025-01-31 17:01:37 +0100

    added cps highscore, improved replaceAll call

--- js/stats/index.js
+++ js/stats/index.js
@@ -2,14 +2,17 @@ import { statsListElem } from '../constants.js';
 import { createElement } from '../utils/index.js';
 
 globalThis.stats = {
-  realClickCount: 0
+  realClickCount: 0,
+  hightestCPS: 0
 };
 
 for (const [k, v] of Object.entries(stats)) {
   createElement(
     'span', { id: 'count-span', textContent: v },
     createElement(
-      'li', { id: k, textContent: k.replaceAll(/[A-Z]/g, e => ' ' + e.toLowerCase()) + ':' },
+
+      // The regexes add spaces before uppercase letters and lowercase said letters. Does not match acronyms.
+      'li', { id: k, textContent: k.replaceAll(/(?<![A-Z])[A-Z]/g, ' $&').replaceAll(/[A-Z][^A-Z]/g, e => e.toLowerCase()) + ':' },
       statsListElem
     )
   );
