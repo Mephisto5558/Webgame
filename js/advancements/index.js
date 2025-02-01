2f28517a1dfb9fd2b3210c623cdbfca45353269c
Mephisto5558 <87113906+Mephisto5558@users.noreply.github.com>
2025-02-01 18:12:21 +0100

    added advancement

--- js/advancements/index.js
+++ js/advancements/index.js
@@ -1,3 +1,4 @@
+import { cpsCountSpan } from '../constants.js';
 import { Advancement } from './advancementClass.js';
 
 const fmt = num => num.toLocaleString();
@@ -9,7 +10,8 @@ const advancements = [
   new Advancement('1TClicks', `You have reached ${fmt(1000)} clicks!`, () => clickCount >= 1000),
   new Advancement('100TClicks', `You have reached ${fmt(100_000)} clicks! Are you bored or sth?`, () => clickCount >= 100_000),
   new Advancement('1MClicks', `You have reached ${fmt(1_000_000)} clicks! Now go touch some grass...`, () => clickCount >= 1_000_000),
-  new Advancement('10PassiveCPS', 'You are now gaining 10 CPS without even doing anything! Lazy.', () => shopItems.autoClick.calc.getIncrease() >= 10)
+  new Advancement('10PassiveCPS', 'You are now gaining 10 CPS without even doing anything! Lazy.', () => shopItems.autoClick.calc.getIncrease() >= 10),
+  new Advancement('HighActiveCPS', 'Are you really autoclicking right now...', () => cpsCountSpan.dataset.value / Math.max(1, shopItems.clickPerClick.calc.getIncrease() - 1) >= 50)
 ];
 
 /** @type {advancements} */
