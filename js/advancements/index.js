b6b6904dc9332cc07a932b305a6bba00487d3413
Mephisto5558 <87113906+Mephisto5558@users.noreply.github.com>
2025-02-01 18:46:48 +0100

    improved code logic

--- js/advancements/index.js
+++ js/advancements/index.js
@@ -11,7 +11,11 @@ const advancements = [
   new Advancement('100TClicks', `You have reached ${fmt(100_000)} clicks! Are you bored or sth?`, () => clickCount >= 100_000),
   new Advancement('1MClicks', `You have reached ${fmt(1_000_000)} clicks! Now go touch some grass...`, () => clickCount >= 1_000_000),
   new Advancement('10PassiveCPS', 'You are now gaining 10 CPS without even doing anything! Lazy.', () => shopItems.autoClick.calc.getIncrease() >= 10),
-  new Advancement('HighActiveCPS', 'Are you really autoclicking right now...', () => cpsCountSpan.dataset.value / Math.max(1, shopItems.clickPerClick.calc.getIncrease() - 1) >= 50)
+  new Advancement(
+    'HighActiveCPS',
+    'Are you really autoclicking right now...',
+    () => cpsCountSpan.dataset.value / Math.max(1, shopItems.clickPerClick.calc.getIncrease() - 1) - shopItems.autoClick.calc.getIncrease() >= 50
+  )
 ];
 
 /** @type {advancements} */
